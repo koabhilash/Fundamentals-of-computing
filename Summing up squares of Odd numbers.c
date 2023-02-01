644229db09b5c1aaa279e478cbd2dff91196b252
koabhilash <124149497+koabhilash@users.noreply.github.com>
2023-02-01 16:54:05 +0530

    Create Summing up squares of Odd numbers.c

---
+++ Summing up squares of Odd numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int NUM,i,j,SUM=0;
+clrscr();
+printf(“\nENTER INTERGER NUMBER : “);
+scanf(“%d”,&NUM);
+for(i=1;i<NUM+1;i++)
+{
+if(i%2!=0)
+{
+SUM=SUM+(i*i);
+}
+}
+printf(“\nTHE SUM OF SQUARE OF ODD NOS. TILL %d NO. IS : %d”,NUM,SUM);
+getch();
+}
